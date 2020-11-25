c28b16df784ba2d53b3eddc7b76a60fdba05a65b
talanova <talanova@speechpro.com>
2020-11-25 15:44:09 +0300

    add hw #4

---
+++ src/hw04/hw#4.js
@@ -0,0 +1,30 @@
+export function f41() {
+  const user = {
+    name: "John",
+  };
+
+  const age = +prompt("Enter user age:");
+  if (isNaN(age) || age < 0) {
+    return null;
+  }
+  user.age = age;
+  return user;
+}
+
+const currentUser = f41();
+console.log(currentUser);
+
+export function f42(user) {
+  const admin = { ...user, role: "admin"};
+  return admin;
+}
+
+const currentAdmin = f42(currentUser);
+console.log(currentAdmin);
+
+export function f43(admin) {
+  const { name, age, role } = admin;
+  console.log(name, age, role);
+}
+
+f43(currentAdmin);
