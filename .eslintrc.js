999794f09c60c7e9b5f5485280966a8fffbc6ecc
talanova <talanova@speechpro.com>
2020-11-25 15:35:49 +0300

    update eslintrc.js and add hw #1

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,5 +9,11 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: "module",
   },
-  rules: {},
+  rules: {
+    "import/prefer-default-export": "off",
+    "jest/valid-title": "off",
+    "no-console": "off",
+    "no-alert": "off",
+    "no-restricted-globals": "off",
+  },
 };
