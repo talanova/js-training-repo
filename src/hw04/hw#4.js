4d5d034136b6ad6a5786cf183c13ef784d32811e
talanova <talanova@speechpro.com>
2020-12-01 22:42:01 +0300

    hw fix: clean code

--- src/hw04/hw#4.js
+++ src/hw04/hw#4.js
@@ -11,20 +11,12 @@ export function f41() {
   return user;
 }
 
-const currentUser = f41();
-console.log(currentUser);
-
 export function f42(user) {
   const admin = { ...user, role: "admin" };
   return admin;
 }
 
-const currentAdmin = f42(currentUser);
-console.log(currentAdmin);
-
 export function f43(admin) {
   const { name, age, role } = admin;
   console.log(name, age, role);
 }
-
-f43(currentAdmin);
--- src/hw05/hw#5.js
+++ src/hw05/hw#5.js
@@ -1,5 +1,3 @@
-const firstArr = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
-
 export function f51(arr) {
   let sum = 0;
   // eslint-disable-next-line no-return-assign
@@ -7,20 +5,13 @@ export function f51(arr) {
   console.log(sum);
 }
 
-f51(firstArr);
-
 export function f52(arr) {
   return arr.map((x) => {
     return x * 2;
   });
 }
 
-const secondArr = f52(firstArr);
-console.log(secondArr);
-
 export function f53(arr) {
   arr.sort((x, y) => x - y);
   console.log(arr.shift(), arr.pop());
 }
-
-f53(firstArr);
